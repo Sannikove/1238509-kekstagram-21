abb15749a29887afe91af281c7aa1b55368bb16c
sannikove <ala00lala@gmail.com>
2020-10-24 12:44:23 +0300

    исправила  debounce

--- js/debounce.js
+++ js/debounce.js
@@ -1,13 +1,18 @@
 'use strict';
-
 (function () {
   const DEBOUNCE_INTERVAL = 500; // ms
 
-  let lastTimeout;
   window.debounce = function (cb) {
-    if (lastTimeout) {
-      window.clearTimeout(lastTimeout);
-    }
-    lastTimeout = window.setTimeout(cb, DEBOUNCE_INTERVAL);
+    let lastTimeout = null;
+
+    return function (...parameters) {
+      if (lastTimeout) {
+        window.clearTimeout(lastTimeout);
+      }
+      lastTimeout = window.setTimeout(function () {
+        cb(...parameters);
+      }, DEBOUNCE_INTERVAL);
+    };
   };
 })();
+
